77ed0a2342bfe3fd4cc5d41297e0e1e2eb59c6d3
Amaka <amaka@fuga.com>
2019-01-30 14:56:10 +0100

    remove unnescessary middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import createHistory from 'history/createBrowserHistory';
 
 import createStore from './redux/create';
 import ApiClient from './helpers/ApiClient';
 import './index.css';
 import App from './containers/App/App';
 
-const history = createHistory();
 const client = new ApiClient();
-const store = createStore(history, client);
+const store = createStore(client);
 
 ReactDOM.render(<Provider store={store}>
   <App />
--- src/redux/create.js
+++ src/redux/create.js
@@ -1,17 +1,14 @@
 import { createStore as _createStore, applyMiddleware } from 'redux';
 import createMiddleware from './middleware/clientMiddleware.js';
-import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
-import asyncMiddleware from 'redux-async';
 import products from './modules/products.js';
 
-export default function createStore(history, client, data) {
-  const reduxRouterMiddleware = routerMiddleware(history);
+export default function createStore(client, data) {
 
-  const middleware = [createMiddleware(client), reduxRouterMiddleware, thunk];
+  const middleware = [createMiddleware(client), thunk];
 
   let finalCreateStore;
-  finalCreateStore = applyMiddleware(...middleware, asyncMiddleware)(_createStore);
+  finalCreateStore = applyMiddleware(...middleware)(_createStore);
 
   const store = finalCreateStore(products, data);
 
